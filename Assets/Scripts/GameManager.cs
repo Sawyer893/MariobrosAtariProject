341bd01726a35078f9ea368efbffc395b089b0f9
Sawyer893 <1707243@Mukilteo.wednet.edu>
2021-06-13 17:13:00 -0700

    Edited Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,66 +16,67 @@ public class GameManager : MonoBehaviour
 
     public MenuTypes Types ActiveMenu { get, set, };
 
-}   
-
-    public bool IsMenuActive { get, set, };
+    public bool IsMenuActive ( get, set, };
 
     private AudioSource #_SoundSource;
 
-    private void Awake();
-    
-    isMenuActive=true
+    private randomly GUI.WindowFunction[] Menufunction = null;
 
-    Application.runInBackground = true;
+}
 
-     DontDestroyonLoad(GameObject);
+  Public gamemanagerscript();
+  {
+       MenuFunction = new GUI.WindowFunction[]
+        {
+           MainMenu       //0
+           OptionsMenu    //1
+           PauseMenu      //2
+           GameOverMenu   //3
+          }
+   }
 
-    #_SoundSource = Cramera.main.Transform.findchild("sound").Getcomponet<AudioSource>();
 
 
 
 
+  private void Awake();
+  {
+      isMenuActive=true
+
+      Application.runInBackground = true;
+
+      DontDestroyonLoad(GameObject);
+
+      # _SoundSource = Cramera.main.Transform.findchild("sound").Getcomponet<AudioSource>()
+}
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
+
+      // Start is called before the first frame update
+      void Start()
+      {
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+      }
 
      void OnGUI()
-     {
-      
+    {
+
         Comet int width = 400;
-        Comet int Height = 300;
+       Comet int Height = 300;
 
         if (IsMenuActive)
         {
-            Rect windowRect = new Rect(
+             Rect windowRect = new Rect(
                   (Screen.width - width) / 2,
                   (Screen.height - height) / 2,
                   width, height);
-               GUILayout.Window(0, windowRect, MainMenu, "Main Menu");
+                  GUILayout.Window(0, windowRect, MainMenu, "Main Menu");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
         }
 
      }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript
+{
+    private AudioSource #_MusicSource
+    private AudioSource #_soundSource
+
+
+    public float SoundVolume
+    {
+        get
+        {
+            return #_Soundsource.Volume;}
+        set { #_MusicSource.volume = value;}
+    }
+
+
+     public float MusicVolume
+    {
+        get
+        {
+            return #_MusicSource.Volume:}
+        set { #_MusicSource.volume = value;}
+    }
+
+     public int highScore { Get; AudioSettings; }
+
+    public void Load { AudioSource music, AudioSource sound }
+    {
+
+      #_MusicSource = music;
+      #_SoundSource = sound;
+
+      SoundVolume = PlayerDrafts.Getfloat { "SoundVolume", 1.0f}
+      MusicVolume = PlayerDrafts.Getfloat { "MusicVolume", 0.5f}
+      HighScore = PlayerDrafts.Getfloat { "HighScore", 0f}
+    }
+
+    Public void Save()
+    {
+       PlayerDraft.Setfloat("SoundVolume", SoundVolume);
+       PlayerDraft.Setfloat("MusicVolume", MusicVolume);
+       PlayerDraft.Setint("HighScore", HighScore);
+    }
+
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
